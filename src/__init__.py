8e52011474c40aca617bda37af1ece86be691797
Hirula <hirulapinibinda01@gmail.com>
2025-10-13 00:45:51 +0530

    updated init

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,1 @@
-
 __version__ = "1.0.0"
-__author__ = "Hirula Abesignha"
-__license__ = "MIT"
-
-# Note: Import specific modules as needed to avoid circular imports
-# Example usage:
-#   from src.utils import CONFIG
-#   from src.model import create_dr_model
